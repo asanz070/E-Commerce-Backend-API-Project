ba0763b9f4c5ae7a37fc516f0d0ca272940d1d79
Alan Sanchez <alsans81@outlook.com>
2025-07-11 13:11:09 -0400

    Finaliazed Controller and Router for Cart with add routes in index file

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCart, getCartByCustomerId, createCart } = require('../controllers/cartController');
+const { getCart, getCartByCustomerId, createCart, addItemToCart, updateItemQuantity, removeItemFromCart, clearCart } = require('../controllers/cartController');
 
 router.get('/', async (request, response) => {
     try {
@@ -27,7 +27,61 @@ router.post('/', async (request, response) => {
         const newCart = await createCart(request.body);
         response.status(200).json({ message: 'success', payload: newCart })
     } catch (error) {
-        response.status(400).json({ message: 'failure', payload: error })
+        response.status(400).json({ message: 'failure', payload: error.message })
+    }
+})
+
+router.post('/:customerId/add', async (request, response) => {
+    try {
+        const { productId, quantity } = request.body;
+        const customerId = request.params.customerId;
+
+        const updatedCart = await addItemToCart(customerId, productId, quantity);
+        response.status(200).json({ message: 'success', payload: updatedCart });
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error.message });
+    }
+});
+
+router.put('/:customerId/update/:productId', async (request, response) => {
+    try {
+        const { quantity } = request.body;
+        const { customerId, productId } = request.params;
+        const updatedCart = await updateItemQuantity(customerId, productId, quantity);
+        response.status(200).json({ message: 'success', payload: updatedCart });
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error.message });
+    }
+});
+
+router.delete('/customerId/remove/:productId', async (request, response) => {
+    try {
+        const { customerId, productId } = request.params;
+        const updatedCart = await removeItemFromCart(customerId, productId);
+        response.status(200).json({ message: 'success', payload: updatedCart })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error.message })
+
+    }
+})
+
+router.delete('/customerId/clear', async (request, response) => {
+    try {
+        const { customerId } = request.params;
+        const clearedCart = await clearCart(customerId)
+        response.status(200).json({ message: 'success', payload: clearedCart })
+    } catch (error) {
+        response.status(400).json({ message:'failure', payload: error.message })
+    }
+})
+
+router.get('/customerId/total', async (request, response) => {
+    try {
+        const { customerId } = request.params;
+        const result = await getCartWithTotal(customerId);
+        response.status(200).json({ message: 'success', payload: result })
+    } catch (error) {
+        response.status(400).json({ message: 'failure', payload: error.message })
     }
 })
 
